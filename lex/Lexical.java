17892d993b140746ae497035aec2073a1fde14d1
jaw653 <jawax10@gmail.com>
2019-01-20 16:06:20 -0500

    added functionality for skipWhiteSpace(), next is to implement regex for comments

--- lex/Lexical.java
+++ lex/Lexical.java
@@ -4,28 +4,78 @@
  * Lexical Analysis Module
  */
 
+ // Is there a way to compile with more warning settings and do I need to do it?
+
+
 import java.io.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 class Lexical
 {
     /**
-     * Lexer to return lexemes
+     * Method to skip over any whitespace in a File
+     * @file The file to be read
+     * returns ArrayList of Character objects from the file excluding whitespace
      */
-    public static int lex(File file)
+    private static ArrayList<Character> skipWhiteSpace(File file) throws IOException
     {
-        char ch;
+        Character ch;
+        ArrayList<Character> input = new ArrayList<Character>();
+
         Scanner sc = new Scanner(file);
 
-        // skipWhiteSpace();
+        String currString = sc.next();
 
-        ch = sc.next.charAt(0);
+        while (sc.hasNext())
+        {
+            int size = currString.length();
+
+            for (int i = 0; i < size; i++)
+            {
+                ch = Character.valueOf(currString.charAt(i));
+                input.add(ch);
+/*
+*** Should not need cause scanner does not take whitespace
+                if ( !(ch.isWhitespace()) )
+                {
+                    input.add(ch);
+                }
+*/
+            }
+
+            currString = sc.next();
+        }
+
+        return input;
+    }
+
+    /**
+     * Lexer to return lexemes
+     */
+    public static int lex(File file) throws IOException
+    {
+        char ch;
+
+        ArrayList<Character> charList = skipWhiteSpace(file);
+
+        for (int i = 0; i < charList.size(); i++)
+        {
+            System.out.println(charList.get(i));
+        }
+        // ch = sc.next.charAt(0);
 
         // if input failed return new lexeme(ENDofINPUT)
 
-        
+        // switch on single character tokens
+        // ie case '(': return new lexeme(OPAREN)
+
+        // default case:
+        // if character.isDig
 
 
+
+        return 1;       //FIXME: placeholder
 
     }
 
@@ -42,14 +92,10 @@ class Lexical
         }
 
         File file = new File(args[0]);
-        Scanner sc = new Scanner(file);
 
-        char ch = sc.next().charAt(0);
-
-        while (sc.hasNext())
+        if ( lex(file) != 1 )
         {
-            System.out.println(ch + " ");
-            ch = sc.next().charAt(0);
+            System.out.println("error");
         }
 
         // System.out.println(args[0]);
