650e5450aa0af42e531c5c29fd876bd390fff8e0
jaw653 <jawax10@gmail.com>
2019-01-21 14:01:44 -0600

    initial commit of barebones Lexeme class

---
+++ lex/Lexeme.java
@@ -0,0 +1,16 @@
+/**
+ * Author: Jake Wachs
+ * CS 403, Programming Languages
+ * The University of Alabama
+ *
+ * Lexeme Class
+ */
+
+class Lexeme
+{
+    String type;
+    String string;
+    int integer;
+    double real;
+    char character;
+}
