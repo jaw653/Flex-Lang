dd00e28ae3227ee7b92a2506dccc8e4e1e734797
jaw653 <jawax10@gmail.com>
2019-01-21 15:08:33 -0600

    added constructors and a getter function as well as more variables that will be needed

--- lex/Lexeme.java
+++ lex/Lexeme.java
@@ -13,4 +13,34 @@ class Lexeme
     int integer;
     double real;
     char character;
+    boolean bool;
+
+    /**
+     * Constructor method
+     * @type The type of the new Lexeme
+     */
+    public Lexeme(String type)
+    {
+        type = this.type;
+    }
+
+    /**
+     * Override constructor
+     * @type The type of the new Lexeme
+     * @str The string of the new Lexeme
+     */
+    public Lexeme(String type, String str)
+    {
+        type = this.type;
+        string = str;
+    }
+
+    /**
+     * Basic getter function for type field
+     * @return The type field of the Lexeme
+     */
+    public String getType()
+    {
+        return type;
+    }
 }
