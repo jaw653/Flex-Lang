dfaf4f7136f114a76344ff2627499f2734da3ac6
jaw653 <jawax10@gmail.com>
2019-01-22 14:54:20 -0600

    uncommitted changes from last session, nothing drastic changed

--- lex/Lexeme.java
+++ lex/Lexeme.java
@@ -29,10 +29,10 @@ class Lexeme
      * @type The type of the new Lexeme
      * @str The string of the new Lexeme
      */
-    public Lexeme(String type, String str)
+    public Lexeme(String type, Character ch)
     {
         type = this.type;
-        string = str;
+        character = ch;
     }
 
     /**
@@ -42,5 +42,13 @@ class Lexeme
     public String getType()
     {
         return type;
+    }
+
+    /**
+     * Displays Lexeme type to stdout
+     */
+    public void display()
+    {
+        System.out.println(type);
     }
 }
