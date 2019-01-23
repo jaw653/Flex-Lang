251081f6bc63f3e6d9afe965a490158532292d60
jaw653 <jawax10@gmail.com>
2019-01-22 20:31:02 -0600

    converted to an interface rather than a class, added (hopefully) all types

--- lex/Types.java
+++ lex/Types.java
@@ -6,7 +6,40 @@
  * Types Module
  */
 
-class Type
+// Is it ok that some of these are actually only characters but are stored as Strings?
+
+interface Types
 {
+    public static final String VAR = "VAR";
+    public static final String REAL = "REAL";
+    public static final String INTEGER = "INTEGER";
+    public static final String CHARACTER = "CHARACTER";
+    public static final String STRING = "STRING";
+    public static final String DEFINE = "DEFINE";
+    public static final String FUNCTION = "FUNCTION";
+    public static final String ID = "ID";            //FIXME: what should this be?
+    public static final String BUNDLE = "BUNDLE";
+    public static final String RETURN = "RETURN";
+    public static final String IF = "IF";
+    public static final String ELSE = "ELSE";
+    public static final String WHILE = "WHILE";
+    public static final String FOR = "FOR";
+    public static final String OPEN_PAREN = "OPEN_PAREN";
+    public static final String CLOSE_PAREN = "CLOSE_PAREN";
+    public static final String COMMA = "COMMA";
+    public static final String PLUS = "PLUS";
+    public static final String MINUS = "MINUS";
+    public static final String TIMES = "TIMES";
+    public static final String DIVIDE = "DIVIDE";
+    public static final String LESS_THAN = "LESS_THAN";
+    public static final String GREATER_THAN = "GREATER_THAN";
+    public static final String EQUAL_TO = "EQUAL_TO";
+    public static final String ASSIGN = "ASSIGN";
+    public static final String SEMICOLON = "SEMICOLON";
+    public static final String MODULO = "MODULO";
+    public static final String UNKNOWN = "UNKNOWN";       //FIXME: what should this be?
+    public static final String ENDofINPUT = "ENDOFINPUT";    //FIXME: what shoud this be?
+    public static final String BADNUM = "BADNUM";
+
 
 }
