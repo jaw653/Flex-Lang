9cb5e58622acc369aa24b010ffe1d27ae7afceed
jaw653 <jawax10@gmail.com>
2019-01-28 22:42:52 -0600

    created new parse directory for parser/recognizer

---
+++ parse/Recognizer.java
@@ -0,0 +1,18 @@
+/**
+ * Author: Jake Wachs
+ * CS 403, Programming Languages
+ * The University of Alabama
+ *
+ * Recognizer Class
+ */
+package parse;
+
+import lex.*;
+
+public class Recognizer implements Types
+{
+    public static void main(String[] args)
+    {
+        Lexeme a = new Lexeme(PLUS);
+    }
+}
