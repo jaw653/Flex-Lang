ee8dbef1287d4e936985e460dd0851e8bf9b4145
jaw653 <jawax10@gmail.com>
2019-01-29 12:03:05 -0600

    copied/pasted checkCmdArgs() and openFile() methods from old Scanner class, wrote main

--- parse/Parser.java
+++ parse/Parser.java
@@ -7,8 +7,58 @@
  */
 package parse;
 
+import java.io.*;
+
+import lex.*;
+
 
 public class Parser
 {
+    /**
+     * Checks to make sure there is a correct number of command line args
+     * @args String array of command line args
+     */
+    private static void checkCmdArgs(String[] args)
+    {
+        if (args.length != 1)
+        {
+            System.out.println("Incorrect number of command line args");
+            System.exit(-1);
+        }
+    }
 
+    /**
+     * Safe method for opening file
+     * @filename The name of the file to be opened
+     * @return The pointer to the opened file object
+     */
+    private static File openFile(String filename) throws IOException
+    {
+        File file = new File(filename);
+
+        if ( !file.exists() )
+        {
+            System.out.println("File does not exist");
+            System.exit(-1);
+        }
+
+        return file;
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        checkCmdArgs(args);
+
+        File file = openFile(args[0]);
+        PushbackInputStream stream = new PushbackInputStream(new FileInputStream(file));
+
+        Lexer i = new Lexer(stream);
+        Lexeme curr = i.lex();
+
+        Recognizer r = new Recognizer(curr, stream);
+
+        r.program();
+
+        stream.close();
+    }
 }
