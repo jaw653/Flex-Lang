b4b910a36fa92a8a5af040f58dbb46e70561635b
jaw653 <jawax10@gmail.com>
2019-02-01 18:17:31 -0600

    added env dir and created Environment class

---
+++ env/Environment.java
@@ -0,0 +1,22 @@
+/**
+ * Author: Jake Wachs
+ * CS 403, Programming Languages
+ * The University of Alabama
+ *
+ * Environment Class
+ */
+
+package env;
+
+//import statements here (lex, recognize)
+
+public class Environment
+{
+    /**
+     * Default Constructor
+     */
+    public Environment()
+    {
+
+    }
+}
