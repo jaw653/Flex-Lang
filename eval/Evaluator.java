89a764e0a23664e8ad5022b952adc57d548d39d3
jaw653 <jawax10@gmail.com>
2019-02-17 16:28:01 -0600

    created evaluator directory and class

---
+++ eval/Evaluator.java
@@ -0,0 +1,25 @@
+/**
+ * Author: Jake Wachs
+ * CS 403, Programming Languages
+ * The University of Alabama
+ *
+ * Evaluator class
+ */
+
+package eval;
+
+import lex.*;
+import parse.*;
+import recognize.*;
+
+public class Evaluator implements Types
+{
+	/**
+	 * Default constructor
+	 */
+	public Evaluator()
+	{
+	}
+
+
+}
